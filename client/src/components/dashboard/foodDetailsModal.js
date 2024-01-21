20c7687beadb29d0e93772ecb96a0c62d02db021
miguel-fg <miguelfierro55@gmail.com>
2024-01-21 10:32:16 -0800

    Removed shopping cart context

--- client/src/components/dashboard/foodDetailsModal.js
+++ client/src/components/dashboard/foodDetailsModal.js
@@ -17,20 +17,9 @@ import {
 } from "grommet";
 import { Basket } from "grommet-icons";
 
-import { useShoppingCart } from "../../context/ShoppingCartContext";
 
 function DetailsModal(props) {
   const [buying, setBuying] = useState(0);
-  const { addToCart } = useShoppingCart();
-
-  const handleAddToCart = () => {
-    const cartItem = {
-      productName: props.name,
-      quantity: buying 
-    };
-
-    addToCart(cartItem);
-  }
 
   return (
     <Card height="large" width="large" background="light-1">
@@ -79,7 +68,6 @@ function DetailsModal(props) {
               label="Add to Basket"
               fill="horizontal"
               icon={<Basket color="white" />}
-              onClick={handleAddToCart}
             />
             <TextInput
               type="number"
--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
-import { ShoppingCartProvider } from "./context/ShoppingCartContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
-      <ShoppingCartProvider>
     <App />
-      </ShoppingCartProvider>
     </AuthContextProvider>
   </React.StrictMode>
 );
