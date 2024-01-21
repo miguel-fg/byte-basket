0ae25d817b7407f74de2b88c98fd5a80b2ab7563
Ansh Gandhi <anshgandhi6sep@gmail.com>
2024-01-21 08:14:32 -0800

    committing to save changes

--- client/byte-basket/src/App.js
+++ client/byte-basket/src/App.js
@@ -3,13 +3,15 @@ import {Grommet} from "grommet";
 // import './App.css';
 import Dashboard from "./dashboard/dashboard";
 import CheckoutGrommet2 from './checkout/checkoutGrommet2';
-
+import CheckoutNew from './checkout/checkout-new';
+import CheckoutNew2 from './checkout/checkout-new-2';
 
 function App() {
   return (
       <Grommet>
         {/* <Dashboard/> */}
-        <CheckoutGrommet2/>
+        {/* <CheckoutGrommet2/> */}
+        <CheckoutNew2/>
       </Grommet>
   );
 }
